4feab1fec6b25c035cc75d19be4c29d919aba48a
Diana Oltean <dyana_mysport@yahoo.com>
2021-03-22 15:38:15 +0000

    EAD Lab 4 Task 1

---
+++ Welcome/Controllers/WelcomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Welcome.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.TimeMessage = "Date: " + DateTime.Now.ToShortDateString() + " Time: " + DateTime.Now.ToShortTimeString();
+            return View();
+        }
+    }
+}
